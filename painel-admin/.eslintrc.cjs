0ba95ec0358eb41ee509c278f18f138a56bb31d7
rerodrigues118-eng <noreply@github.com>
2026-07-08 23:33:37 -0300

    Remove painel-admin: repositorio agora contem apenas o app principal RankHire

--- painel-admin/.eslintrc.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  extends: ["next/core-web-vitals", "next/typescript"],
-  ignorePatterns: [".next/**", "node_modules/**", "out/**", "dist/**"],
-};
--- painel-admin/scripts/check-node-version.js
+++
@@ -1,8 +0,0 @@
-const semver = process.version.replace(/^v/, '');
-const major = Number(semver.split('.')[0]);
-if (major < 18) {
-  console.error(`Node ${process.version} detected. RankHire requires Node 18+. Please upgrade Node.`);
-  process.exitCode = 1;
-} else {
-  console.log(`Node ${process.version} OK.`);
-}
